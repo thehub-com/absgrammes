3cd2ab62e72d5e58917b403818d791dab82f291b
thehub-com <korada209@gmail.com>
2026-01-08 12:12:37 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,16 @@
+// config.js
+window.ABSGRAM_CONFIG = {
+  supabase: {
+    url: "https://zdmtwnvaksdbvutrpcnr.supabase.co",
+    anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpkbXR3bnZha3NkYnZ1dHJwY25yIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc1Mjg4NjcsImV4cCI6MjA4MzEwNDg2N30.QztruYbzPeF8CrZmT_FhMw6VHc1-289qqJ8Qs4Z7nVc"
+  },
+  app: {
+    name: "Absgram",
+    version: "1.0",
+    colors: {
+      primary: "#FF9800",
+      white: "#FFFFFF",
+      black: "#000000"
+    }
+  }
+};
